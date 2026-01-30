6abfcc4b7fddbe2ff033749d5c4b012dd6f51429
Beknazar Sarsenbek <sarsenbekbeknazar45@gmail.com>
2026-01-30 12:32:58 +0500

    Fix model require path

--- routes/items.js
+++ routes/items.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Item = require("../models/Item");
+const Item = require("../models/item");
 
 const router = express.Router();
 
